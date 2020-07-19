750ae0a48f15585a26b454e2e3886cad4ab0163c
jimzers <38634441+jimzers@users.noreply.github.com>
2020-07-18 20:33:13 -0700

    add google analytics tracking

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import './App.css';
 
 import Main from './Main'
 
+import ReactGA from 'react-ga';
+
 function App() {
+    ReactGA.initialize('UA-172945829-2');
+    ReactGA.pageview('/');
+    
     return (
         <div className="App">
             <Main/>
